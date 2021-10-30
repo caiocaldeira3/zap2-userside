28bcc595303587486123c345c248931e2818964f
Caio Caldeira <caio.caldeira3@gmail.com>
2021-10-30 03:02:47 -0300

    Add cryptography functions as well as well as a python script to test it

---
+++ chatapp/user/app/util/crypto.py
@@ -0,0 +1,101 @@
+import base64
+
+from typing import Union
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.backends.openssl.x25519 import _X25519PrivateKey, _X25519PublicKey
+from cryptography.hazmat.backends import default_backend
+
+from Crypto.Cipher import AES
+
+class SymmetricRatchet(object):
+    def __init__ (self, key: bytes):
+        self.state = key
+
+    def next (self, inp: bytes = b"", init_vector: bytes = b"") -> bytes:
+        output = hkdf_derive_key(self.state + inp, 80)
+        self.state = output[ : 32 ]
+
+        # key, initializing vector
+        return output[ 32 : 64 ], output[ 64: ]
+
+def decode_b64 (msg: bytes) -> str:
+    return base64.encodebytes(msg).decode("utf-8").strip()
+
+def pad (msg: bytes) -> bytes:
+    # pkcs7 padding
+    num = 16 - (len(msg) % 16)
+    return msg + bytes([num] * num)
+
+def unpad (msg: bytes) -> bytes:
+    # remove pkcs7 padding
+    return msg[ : -msg[-1] ]
+
+def hkdf_derive_key (input: bytes, length: int) -> bytes:
+    hkdf_key = HKDF(
+        algorithm=hashes.SHA256(), length=length, salt=b"", info=b"", backend=default_backend()
+    )
+
+    return hkdf_key.derive(input)
+
+def sender_x3dh (
+    sender_keys: dict[str, _X25519PrivateKey], recv_keys: dict[str, _X25519PublicKey]
+) -> bytes:
+    dh1 = sender_keys["IK"].exchange(recv_keys["SPK"])
+    dh2 = sender_keys["EK"].exchange(recv_keys["IK"])
+    dh3 = sender_keys["EK"].exchange(recv_keys["SPK"])
+    dh4 = sender_keys["EK"].exchange(recv_keys["OPK"])
+
+    return hkdf_derive_key(dh1 + dh2 + dh3 + dh4, 32)
+
+def receiver_x3dh (
+    sender_keys: dict[str, _X25519PublicKey], recv_keys: dict[str, _X25519PrivateKey]
+) -> bytes:
+    dh1 = recv_keys["SPK"].exchange(sender_keys["IK"])
+    dh2 = recv_keys["IK"].exchange(sender_keys["EK"])
+    dh3 = recv_keys["SPK"].exchange(sender_keys["EK"])
+    dh4 = recv_keys["OPK"].exchange(sender_keys["EK"])
+
+    return hkdf_derive_key(dh1 + dh2 + dh3 + dh4, 32)
+
+def init_root_ratchet (shared_key: bytes) -> SymmetricRatchet:
+    return SymmetricRatchet(shared_key)
+
+Ratchet = Union[SymmetricRatchet, X25519PrivateKey]
+
+def dh_ratchet_rotation_receive (
+    ratchets: dict[str, Ratchet], pbkey: bytes
+) -> None:
+    dh_recv = ratchets["dh_ratchet"].exchange(pbkey)
+    shared_recv = ratchets["root_ratchet"].next(dh_recv)[0]
+
+    ratchets["rcv_ratchet"] = SymmetricRatchet(shared_recv)
+
+def dh_ratchet_rotation_send (
+    ratchets: dict[str, Ratchet], pbkey: bytes
+) -> None:
+    ratchets["dh_ratchet"] = X25519PrivateKey.generate()
+    dh_send = ratchets["dh_ratchet"].exchange(pbkey)
+    shared_send = ratchets["root_ratchet"].next(dh_send)[0]
+
+    ratchets["snd_ratchet"] = SymmetricRatchet(shared_send)
+
+def snd_msg (
+    ratchets: dict[str, Ratchet], pbkey: bytes, msg: bytes
+) -> tuple[bytes, _X25519PublicKey]:
+    dh_ratchet_rotation_send(ratchets, pbkey)
+
+    key, init_vector = ratchets["snd_ratchet"].next()
+    cipher = AES.new(key, AES.MODE_CBC, init_vector).encrypt(pad(msg))
+
+    return cipher, ratchets["dh_ratchet"].public_key()
+
+def rcv_msg (
+    ratchets: dict[str, Ratchet], pbkey: _X25519PublicKey, cipher: bytes
+) -> bytes:
+    dh_ratchet_rotation_receive(ratchets, pbkey)
+    key, init_vector = ratchets["rcv_ratchet"].next()
+
+    return unpad(AES.new(key, AES.MODE_CBC, init_vector).decrypt(cipher))
---
+++ chatapp/user/app/util/crypto_tester.py
@@ -0,0 +1,84 @@
+from base64 import encode
+from cryptography.hazmat.backends.openssl.x25519 import _X25519PrivateKey, _X25519PublicKey
+
+from crypto import *
+
+class keyzeds (object):
+
+    def private_keys (self) -> dict[str, _X25519PrivateKey]:
+        return self.__dict__
+
+    def public_keys (self) -> dict[str, _X25519PublicKey]:
+        return {
+            attr: self.__getattribute__(attr).public_key()
+            for attr in self.__dict__.keys()
+            if isinstance(self.__getattribute__(attr), _X25519PrivateKey)
+        }
+
+class Bob(keyzeds):
+    def __init__(self):
+        # generate Bob's keys
+        self.IK = X25519PrivateKey.generate()
+        self.SPK = X25519PrivateKey.generate()
+        self.OPK = X25519PrivateKey.generate()
+
+        self.dh_ratchet = X25519PrivateKey.generate()
+
+class Alice(keyzeds):
+    def __init__(self):
+        # generate Alice's keys
+        self.IK = X25519PrivateKey.generate()
+        self.EK = X25519PrivateKey.generate()
+
+        self.dh_ratchet = None
+
+al = Alice()
+bob = Bob()
+
+sk = sender_x3dh(al.private_keys(), bob.public_keys())
+sk_ = receiver_x3dh(al.public_keys(), bob.private_keys())
+
+rta = init_root_ratchet(sk)
+rtb = init_root_ratchet(sk_)
+
+ratchets_alice = {
+    "root_ratchet": rta
+}
+ratchets_bob = {
+    "root_ratchet": rtb,
+    "dh_ratchet": bob.dh_ratchet
+}
+
+print(
+    "The order of operation is important.\nThe " +
+    "first message sent must be from alice, or else " +
+    "it wont be able to decrypt the messages."
+)
+
+while True:
+    sender = input("who's sending? [alice, bob] (exit to leave)\n")
+    if sender == "exit":
+        break
+
+    message = input("write the message (up to 16 characters)\n")
+    if sender == "alice":
+        ratchets_sender = ratchets_alice
+        ratchets_receiver = ratchets_bob
+    else:
+        ratchets_sender = ratchets_bob
+        ratchets_receiver = ratchets_alice
+
+    cipher, snd_pbkey = snd_msg(
+        ratchets_sender, ratchets_receiver["dh_ratchet"].public_key(), bytes(message, encoding="utf-8")
+    )
+    print(f"encrypted message: {decode_b64(cipher)}")
+
+    msg = rcv_msg(ratchets_receiver, snd_pbkey, cipher)
+    print(f"decrypted_message: {str(msg)}")
+
+    if sender == "alice":
+        ratchets_alice = ratchets_sender
+        ratchets_bob = ratchets_receiver
+    else:
+        ratchets_alice = ratchets_receiver
+        ratchets_bob = ratchets_sender
