c5929d7f119a96d2a517b68d83f38fbb63207a2c
Caio Caldeira <caio.caldeira3@gmail.com>
2021-11-01 16:36:16 -0300

    Update authentication method and information saved on signup

--- chatapp/server/app/modules/auth/controller.py
+++ chatapp/server/app/modules/auth/controller.py
@@ -7,11 +7,9 @@ from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 from app.util.json_encoder import AlchemyEncoder
 from app.util.responses import AuthorizationError, DuplicateError, NotFoundError, ServerError
 
-# Import password / encryption helper tools
-from werkzeug.security import check_password_hash, generate_password_hash
-
 # Import module models (i.e. User)
 from app.models.user import User
+from app.models.public_keys import OTKey
 
 # Import application Database
 from app import db
@@ -19,67 +17,41 @@ from app import db
 # Define the blueprint: "auth", set its url prefix: app.url/auth
 mod_auth = Blueprint("auth", __name__, url_prefix="/auth")
 
-# Set the route and accepted methods
-@mod_auth.route("/signin/", methods=["PUT"])
-def signin () -> wrappers.Response:
-    try:
-        data = request.json
-        user = User.query.filter_by(email=data["email"]).one()
-
-        if user and check_password_hash(user.password, data["password"]):
-            return Response(
-                response=json.dumps({
-                    "user": user.user_id,
-                    "name": user.name
-                }),
-                status=200,
-                mimetype="application/json"
-            )
-
-        else:
-            return AuthorizationError
-
-    except MultipleResultsFound:
-        return ServerError
-
-    except NoResultFound:
-	    return NotFoundError
-
-    except Exception as exc:
-        print(exc)
-
-        return AuthorizationError
-
 @mod_auth.route("/signup/", methods=["POST"])
 def signup () -> wrappers.Response:
     try:
-        data = request.json
+        data = json.loads(request.json)
 
-        stmt = db.insert(User).values(
+        user = User(
             email=data["email"],
             name=data["name"],
-            password=generate_password_hash(data["password"]),
+            id_key=data["id_key"],
+            sgn_key=data["sgn_key"],
+            otkeys=[
+                OTKey(key_id=otkey["id"], otkey=otkey["key"])
+                for otkey in data["otkeys"]
+            ],
             telephone=data.get("telephone"),
             description=data.get("description", None)
         )
+        db.session.add(user)
+        db.session.add_all(user.otkeys)
+        db.session.commit()
 
-        with db.engine.connect() as connection:
-            result = connection.execute(stmt)
-            query = User.query.filter_by(user_id=result.lastrowid).one()
-
-            return Response(
-                response=json.dumps(query, cls=AlchemyEncoder),
-                status=200,
-                mimetype="application/json"
-            )
+        return Response(
+            response=json.dumps({
+                "status": "ok",
+                "user": {
+                    "id": user.id,
+                    "email": user.email
+                }
+            }, cls=AlchemyEncoder),
+            status=200,
+            mimetype="application/json"
+        )
 
     except IntegrityError as exc:
         print(exc)
 
         return DuplicateError
-
-    except Exception as exc:
-        print(exc)
-
-        return ServerError
 
