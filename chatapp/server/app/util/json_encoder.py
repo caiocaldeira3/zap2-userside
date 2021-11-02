bc3329df7dccc68c70477bbed14f86c08f1bf218
Caio Caldeira <caio.caldeira3@gmail.com>
2021-11-02 02:21:12 -0300

    Add encoder for complex structures with JSON

--- chatapp/server/app/util/json_encoder.py
+++ chatapp/server/app/util/json_encoder.py
@@ -32,3 +32,10 @@ class AlchemyEncoder(json.JSONEncoder):
             return fields
 
         return json.JSONEncoder.default(self, obj)
+
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj,'reprJSON'):
+            return obj.reprJSON()
+        else:
+            return json.JSONEncoder.default(self, obj)
