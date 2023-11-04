9bef85f3733296be7852f27d4e6753ad96826aa6
Caio Alves Caldeira <caio.caldeira@most.com.br>
2023-11-03 21:52:34 -0300

    Add tests for api signup

--- tests/api/test_init.py
+++ tests/api/test_init.py
@@ -1,8 +1,12 @@
+import base64
 from unittest.mock import patch
 
 import pytest
 import socketio
 from bson import ObjectId
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from socketio.exceptions import ConnectionError
 
 from app.models.user import User
 from app.util import config
@@ -166,3 +170,102 @@ def test_api_method_calls_after_logout () -> None:
 
         mock_disconnect.assert_called_once()
 
+def test_api_signup () -> None:
+    pvt_key = X25519PrivateKey.generate()
+
+    with (
+        patch("app.util.crypto.generate_private_key") as mock_generate_private_key,
+        patch("app.util.crypto.sign_message") as mock_sign_message,
+        patch("app.services.user.insert_user") as mock_insert_user,
+        patch("app.util.config.update_user_id") as mock_update_user_id,
+        patch.object(socketio.Client, "connect") as mock_connect
+    ):
+        mock_generate_private_key.return_value = pvt_key
+
+        api = Api()
+        api.signup("name", "telephone", "password")
+
+        assert isinstance(api.user_id, ObjectId)
+        assert api.id_key == pvt_key
+        assert api.sgn_key == pvt_key
+        assert api.ed_key == pvt_key
+
+def test_api_signup_connect_parameters () -> None:
+    pvt_key = X25519PrivateKey.generate()
+    pub_key = base64.encodebytes(
+        pvt_key.public_key().public_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw
+        )
+    ).decode("utf-8").strip()
+
+    with (
+        patch("app.util.crypto.generate_private_key") as mock_generate_private_key,
+        patch("app.util.crypto.sign_message") as mock_sign_message,
+        patch("app.services.user.insert_user") as mock_insert_user,
+        patch("app.util.config.update_user_id") as mock_update_user_id,
+        patch.object(socketio.Client, "connect") as mock_connect
+    ):
+        mock_generate_private_key.return_value = pvt_key
+
+        api = Api()
+        api.signup("name", "telephone", "password")
+
+        mock_connect.assert_called_once_with(
+            "http://0.0.0.0:5000",
+            auth={
+                "name": "name",
+                "telephone": "telephone",
+                "id_key": pub_key,
+                "sgn_key": pub_key,
+                "ed_key": pub_key,
+                "opkeys": [ {
+                    "id": idx,
+                    "key": pub_key
+                } for idx in range(1, 11) ]
+            },
+            headers={ "Param-Auth": config.CHAT_SECRET }
+        )
+
+def test_api_signup_user_headers () -> None:
+    pvt_key = X25519PrivateKey.generate()
+
+    with (
+        patch("app.util.crypto.generate_private_key") as mock_generate_private_key,
+        patch("app.util.crypto.sign_message") as mock_sign_message,
+        patch("app.services.user.insert_user") as mock_insert_user,
+        patch("app.util.config.update_user_id") as mock_update_user_id,
+        patch.object(socketio.Client, "connect") as mock_connect
+    ):
+        mock_sign_message.return_value = True
+        mock_generate_private_key.return_value = pvt_key
+
+        api = Api()
+        api.signup("name", "telephone", "password")
+
+        assert api.headers_user == {
+            "Param-Auth": config.CHAT_SECRET,
+            "Signed-Message": True
+        }
+
+def test_api_signup_rollback () -> None:
+    pvt_key = X25519PrivateKey.generate()
+
+    with (
+        patch("app.util.crypto.generate_private_key") as mock_generate_private_key,
+        patch("app.util.crypto.sign_message") as mock_sign_message,
+        patch("app.services.user.insert_user") as mock_insert_user,
+        patch("app.services.user.delete_user") as mock_delete_user,
+        patch.object(socketio.Client, "connect") as mock_connect
+    ):
+        mock_generate_private_key.return_value = pvt_key
+        mock_connect.side_effect = ConnectionError
+
+        api = Api()
+        api.signup("name", "telephone", "password")
+
+        assert api.user_id is None
+        assert api.id_key is None
+        assert api.sgn_key is None
+        assert api.ed_key is None
+        mock_delete_user.assert_called_once()
